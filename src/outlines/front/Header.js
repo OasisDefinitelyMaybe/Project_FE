2146604ffcca1b8baa5b5f41d8067fc63785118f
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-02-16 14:34:43 +0900

    헤더 작성중

--- src/outlines/front/Header.js
+++ src/outlines/front/Header.js
@@ -1,14 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next'; 
+import { FaUserPlus } from "react-icons/fa";
+import { FiLogIn, FiSearch } from "react-icons/fi";
+import logo from '../../images/logo.png';
 
 const HeaderBox = styled.header`
   background: #fff;
+
+  .layout-width {
+    display: flex;
+    align-items: center;
+    height: 115px;
+
+    .logo,
+    form,
+    .links {
+      flex-grow: 1;
+      width: 0;
+    }
+
+    form {
+      text-align: center;
+    }
+
+    .links {
+      text-align: right;
+
+      a {
+        margin-left: 15px;
+      }
+
+      svg {
+        font-size: 2.25rem;
+      }
+    }
+  }
 `;
 
 const Header = () => {
+    const { t } = useTranslation();
   return (
     <HeaderBox>
-      <div className="layout-width"></div>
+      <div className="layout-width">
+        <Link to="/" className="logo">
+            <img src={logo} alt="logo" />
+        </Link>
+
+        <form>
+          <input type='text' name='skey' placeholder={t('검색어_입력')} />
+          <button type='submit'>
+            <FiSearch />
+          </button>
+        </form>
+
+        <div class="links">
+        <Link to="/member/login">
+            <FiLogIn />
+        </Link>
+        <Link to="/member/join">
+            <FaUserPlus />
+        </Link>
+        </div>
+      </div>
     </HeaderBox>
   );
 };
