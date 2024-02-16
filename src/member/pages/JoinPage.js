66b5ff96121b1986fa50eef8e8a9f90f14329e51
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-02-16 15:52:12 +0900

    회원가입_로그인

---
+++ src/member/pages/JoinPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { useTranslation } from "react-i18next";
+import JoinContainer from "../containers/JoinContainer";
+import { MainTitle } from "../../commons/components/TitleStyle";
+
+const JoinPage = () => {  
+    const { t } = useTranslation();
+
+  return( 
+      <>
+     <Helmet>
+      <title>{t('회원가입')}</title>
+      </Helmet>
+      <MainTitle>{t('회원가입')}</MainTitle>
+      <JoinContainer />
+     </>
+  );
+};
+
+export default React.memo(JoinPage);
