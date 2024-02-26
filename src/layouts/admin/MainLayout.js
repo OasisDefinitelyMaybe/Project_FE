b09f9b1286c31efd8a45547fcb060b7bbd42f5ba
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-02-26 16:15:01 +0900

    메인페이지_메인레이아웃 수정

--- src/layouts/admin/MainLayout.js
+++ src/layouts/admin/MainLayout.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import AdminOnlyContainer from '../../commons/containers/AdminOnlyContainer';
+import { Outlet } from 'react-router-dom';
+import Header from '../../outlines/admin/Header';
+import Side from '../../outlines/admin/Side';
+import SubMenu from '../../outlines/admin/SubMenu';
+
+const ContentBox = styled.main``;
 
 const MainLayout = () => {
-
+    return (
+        <AdminOnlyContainer>
+          <Header />
+          <ContentBox>
+            <Side />
+            <section>
+              <SubMenu />
+              <Outlet />
+            </section>
+          </ContentBox>
+        </AdminOnlyContainer>
+      );
 };
 
 export default React.memo(MainLayout);
