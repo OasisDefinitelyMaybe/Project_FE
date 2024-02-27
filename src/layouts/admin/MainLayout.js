fc426a09d7fc4aee29f3020b6995d34c50f4d68f
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-02-27 09:38:27 +0900

    관리자페이지 작성중

--- src/layouts/admin/MainLayout.js
+++ src/layouts/admin/MainLayout.js
@@ -6,21 +6,32 @@ import Header from '../../outlines/admin/Header';
 import Side from '../../outlines/admin/Side';
 import SubMenu from '../../outlines/admin/SubMenu';
 
-const ContentBox = styled.main``;
+const ContentBox = styled.main`
+  display: flex;
+  background: #f8f8f8;
+  aside {
+    width: 220px;
+    background: #ccc;
+  }
+
+  section.main_content {
+    flex-grow: 1;
+  }
+`;
 
 const MainLayout = () => {
-    return (
-        <AdminOnlyContainer>
-          <Header />
-          <ContentBox>
-            <Side />
-            <section>
-              <SubMenu />
-              <Outlet />
-            </section>
-          </ContentBox>
-        </AdminOnlyContainer>
-      );
+  return (
+    <AdminOnlyContainer>
+      <Header />
+      <ContentBox>
+        <Side />
+        <section className="main_content">
+          <SubMenu />
+          <Outlet />
+        </section>
+      </ContentBox>
+    </AdminOnlyContainer>
+  );
 };
 
 export default React.memo(MainLayout);
