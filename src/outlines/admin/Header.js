9c5645b8f8ddad34927dc93e7af57386f3d9119d
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-02-27 10:19:45 +0900

    관리자 페이지 작성

--- src/outlines/admin/Header.js
+++ src/outlines/admin/Header.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useContext, useCallback } from 'react';
+import styled from 'styled-components';
+import { Link, useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import logo from '../../images/logo.png';
+import { SmallButton } from '../../commons/components/ButtonStyle';
+import UserContext from '../../member/modules/UserContext';
+import { logout } from '../../member/apis/apiLogin';
+
+const HeaderBox = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100px;
+  padding: 0 20px;
+`;
 
 const Header = () => {
-  return <h1>헤더</h1>;
+  const { t } = useTranslation();
+  const { state: isLogin } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const onLogout = useCallback(() => {
+    logout(UserContext);
+    navigate('/member/login?redirectURL=/admin');
+  }, [navigate]);
+
+  return (
+    <HeaderBox>
+      <Link to="/admin" className="logo">
+        <img src={logo} alt="logo" />
+      </Link>
+
+      <div className="links">
+        {isLogin ? (
+          <>
+            <SmallButton
+              width="120px"
+              bcolor="primary"
+              fcolor="#fff"
+              onClick={onLogout}
+            >
+              {t('로그아웃')}
+            </SmallButton>
+          </>
+        ) : (
+          <>
+            <Link to="/member/login">
+              <SmallButton width="120px">{t('로그인')}</SmallButton>
+            </Link>
+            <Link to="/member/join">
+              <SmallButton width="120px">{t('회원가입')}</SmallButton>
+            </Link>
+          </>
+        )}
+      </div>
+    </HeaderBox>
+  );
 };
 
 export default React.memo(Header);
